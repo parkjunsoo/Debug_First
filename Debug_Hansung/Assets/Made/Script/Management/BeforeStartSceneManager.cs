093f4eab5f7f80dee72180f8a376c247cc6b168b
jspark <junsoo76@daum.net>
2017-09-18 22:59:35 +0900

    0918Commit
    노트북. Intro화면 및 BeforeGameStartScene 구현

---
+++ Debug_Hansung/Assets/Made/Script/Management/BeforeStartSceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeforeStartSceneManager : MonoBehaviour {
+
+    GameObject player;
+    float time;
+    float fadeTime;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        player.transform.position = new Vector3(0f, 0f, 0f);
+        player.GetComponent<PlayerControl>().isPaused = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        player.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            fadeTime = time;
+            GameObject.Find("FadeManager").GetComponent<FadeManager>().Fade(true, 1.25f);
+        }
+
+        if (time - fadeTime >= 1.25f && fadeTime != 0)
+        {
+            player.GetComponent<PlayerControl>().isPaused = false;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("FirstFloor");
+        }
+    }
+}
--- Debug_Hansung/Assets/Made/Script/Management/IntroManager.cs
+++ Debug_Hansung/Assets/Made/Script/Management/IntroManager.cs
@@ -5,13 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class IntroManager : MonoBehaviour {
 
+    FadeManager fade;
+    PlayerControl player;
+
 	// Use this for initialization
 	void Start () {
-        SceneManager.LoadScene("FirstFloor");
+        player = GameObject.Find("Player").GetComponent<PlayerControl>();
+        fade = GameObject.Find("FadeManager").GetComponent<FadeManager>();
+        StartCoroutine(Intro());
+        player.isPaused = true;
+        //SceneManager.LoadScene("FirstFloor");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    IEnumerator Intro()
+    {
+        yield return new WaitForSeconds(2f);
+        fade.Fade(true, 2f);
+        yield return new WaitForSeconds(2f);
+        player.isPaused = false;
+        SceneManager.LoadScene("BeforeStartScene");
+    }
 }
