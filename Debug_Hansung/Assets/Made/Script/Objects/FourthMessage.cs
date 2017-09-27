7d6caab535a8744204dbc8d7f085296dbcbd708a
parkjunsoo <junsoo76@daum.net>
2017-09-27 19:55:14 +0900

    0927 Commit
    중간커밋
    4번 쪽지, 화장실 및 1층

---
+++ Debug_Hansung/Assets/Made/Script/Objects/FourthMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourthMessage : Message {
+
+	// Use this for initialization
+	void Start () {
+        subTitle = "이벤트? 뭐 이따위 이벤트가 다있어";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
